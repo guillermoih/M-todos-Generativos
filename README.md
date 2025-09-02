13c17ae046d36cc5e79df802dd027667c6008068
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2025-09-02 17:57:43 +0000

    index.md generator

---
+++ README.md
@@ -0,0 +1,10 @@
+# M-todos-Generativos
+
+## Entorno
+
+```
+uv venv
+source .venv/bin/activate
+uv pip install -r requirements.txt
+```
+
--- genera_index.sh
+++ genera_index.sh
@@ -3,12 +3,21 @@
 echo -e "# Índica de contenidos\n" > index.md
 
 URL="https://guillermoih.github.io/M-todos-Generativos/"
-PDFS=$(ls ./Slides/*.tex)
+URLCOLAB="https://colab.research.google.com/github/guillermoih/M-todos-Generativos/blob/main/Notebooks"
+
 
 echo -e "## Diapositivas\n" >> index.md
+PDFS=$(ls ./Slides/*.tex)
 
 for pdf in $PDFS; do
     name=$(basename "$pdf" .tex)
     echo "- [$name](./$name.pdf)" >> index.md
 done
 
+echo -e "## Notebooks\n" >> index.md
+NOTEBOOKS=$(ls ./Notebooks/*.ipynb)
+
+for notebook in $NOTEBOOKS; do
+    name=$(basename "$notebook" .ipynb)
+    echo "- [$name]($URLCOLAB/$name.ipynb)" >> index.md
+done
